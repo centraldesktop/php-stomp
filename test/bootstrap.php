5b02c2c532139a6eb1e04fb38e888c1f713bab9f
Trey Hyde <thyde@centraldesktop.com>
2013-04-18 14:39:54 -0700

    Fix "return from include" ... silly PHP

--- test/bootstrap.php
+++ test/bootstrap.php
@@ -1,7 +1,6 @@
 <?php
 
 $autoload_path =  __DIR__ . "/../vendor/autoload.php";
-require_once($autoload_path);
+$loader = require($autoload_path);
 
-$loader =   ComposerAutoloaderInit::getLoader();
 $loader->add('CentralDesktop\Stomp\Test', __DIR__);
